53b2216a80c155aeff89cbee43476362d4442307
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2022-05-21 18:29:26 -0700

    created superuser

--- myproject1/urls.py
+++ myproject1/urls.py
@@ -19,5 +19,6 @@ from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('boards/<int:pk>/$', views.board_topics, name='board_topics'),
 ]
